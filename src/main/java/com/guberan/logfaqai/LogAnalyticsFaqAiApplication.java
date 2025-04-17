68a0d57e8becedee48e651620afe79e1b61fb511
Lucien <lucien@MiniMac.local>
2025-04-17 09:59:07 +0200

    initial commit - Spring Boot

---
+++ src/main/java/com/guberan/logfaqai/LogAnalyticsFaqAiApplication.java
@@ -0,0 +1,13 @@
+package com.guberan.logfaqai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnalyticsFaqAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnalyticsFaqAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guberan/logfaqai/LogAnalyticsFaqAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guberan.logfaqai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAnalyticsFaqAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
